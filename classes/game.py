ea6fbdca5b5757249f32fd45d966751dbbd98162
MukhriddinMike <miwady@mail.ru>
2019-02-03 18:53:57 +0900

    making game

---
+++ classes/game.py
@@ -0,0 +1,36 @@
+import random
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+class Person:
+    def __init__(self, hp, mp, atk, df, magic):
+        self.maxhp = hp
+        self.hp = hp
+        self.maxmp = mp
+        self.mp = mp
+        self.atkl = atk - 10
+        self.atkh = atk + 10
+        self.df = df
+        self.magic = magic
+        self.actions = ["Attack", "Magic"]
+
+    #we ned utiliyies
+    # to give some damage and we use random for this
+    def generate_damage(self):
+        return random.randrange(self.atkl,self.atkh)
+
+
+    # spell damage
+    #here creating main
+    def generate_spell_damage(self,i): # i is index number to pass arrays
+        mgl = self.magic[i]['dmg']-5
+        mgh = self.magic[i]['dmg']+5
+        return random.randrange(mgl,mgh)
